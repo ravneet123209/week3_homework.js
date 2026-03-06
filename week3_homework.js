ad829f5cfc57b13c65907e991158b7ccbee04a72
ravneet123209 <harpreetrandhawa70777@gmail.com>
2026-03-06 13:08:54 +0100

    Task 2 sum array and average

--- week3_homework.js
+++ week3_homework.js
@@ -7,3 +7,15 @@ for(let i =1; i<=50; i++){
         console.log(i)
     }
 }
+//Task 2 — Sum Array
+let array = [1,2,3,4,5] 
+let sum = 0; //for storing value of sum
+
+
+//for loop
+for (let i = 0 ; i<= array.length-1; i ++){
+    
+    sum+=array[i]; //adding array to sum
+    
+}
+console.log("sum =",sum, "average = " , sum/array.length);
